f6ee2f15f2b7b242697d23c449aaff4808e6192e
MuriloObr <muriloobregao@gmail.com>
2025-07-19 10:43:37 -0300

    virtual lines nvim update

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,54 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"ts_ls",
+          "eslint",
+					"ruff",
+					"pyright",
+					"tailwindcss",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.eslint.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.ruff.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tailwindcss.setup({
+				capabilities = capabilities,
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Lsp hover" })
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Lsp definition" })
+			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float, {})
+		end,
+	},
+}
---
+++ lua/vim-options.lua
@@ -0,0 +1,11 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set number")
+vim.cmd("set clipboard+=unnamedplus")
+vim.diagnostic.config({
+    -- virtual_lines = true,
+    virtual_text = true,
+
+})
